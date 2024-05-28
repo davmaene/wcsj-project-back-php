5e723e73cdfd6791624d0af37652955ce54e7311
davmaene <davidmened@gmail.com>
2024-05-28 15:12:50 +0200

    up me line

--- models/model.approvisionnement.php
+++ models/model.approvisionnement.php
@@ -50,8 +50,7 @@ class Approvisionnements
 
         if ($conn) {
             try {
-                for ($i = 0; $i < $items; $i++) {
-                    $value = $items[$i];
+                foreach ($items as $key => $value) {
                     $this->__constructor(
                         $value['item'],
                         $value['prix'],
@@ -86,17 +85,7 @@ class Approvisionnements
                                     $stock_details = new Stockdetails($stock_raw, $this->marque, $this->pays_origin, $this->dosage, $this->paquetage, $this->nature, $this->num_lot);
                                     $stock_details = $stock_details->create($config);
                                     if (is_numeric($stock_details)) {
-                                        echo ("=====================================================");
-                                        array_push(
-                                            $saved_items,
-                                            [
-                                                "saved" => get_object_vars($this),
-                                                "stock" => $stock_raw,
-                                                "stock-input" => $stock_input_raw,
-                                                "stock-input-depot" => $stock_input_depot,
-                                                "stock-input-details" => $stock_details,
-                                            ]
-                                        );
+                                        array_push($saved_items, get_object_vars($this));
                                     } else {
                                         return "55 stock_details => " . $stock_details; // step stock details not succeded
                                     }
